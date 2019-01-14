b5d772d4984dc3f1fb710328df0df00395c46a05
Mikhail Pyatakhin <mikhail.pyatakhin@ashbury.ca>
2019-01-14 16:49:12 -0500

    first commit

---
+++ Desktop/AnagramSolver/src/AnagramSolver.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class AnagramSolver {
+
+    public AnagramSolver(){
+
+    }
+
+    private String rearrange(String input){
+        for (int i = 0; i< input.length() * )
+    }
+
+    public boolean check(String input){
+
+    }
+
+    private String search(String wordChecked){
+
+    }
+
+    private int variations(String word){
+        char currentLetter;
+        int repeatedLetters = 0;
+        for (int i = 0; i < word.length(); i++){
+
+            currentLetter = word.charAt(i);
+
+            for (int j = 0; j< word.length() - 1; j++){
+                if (j == i){
+
+                }else if (currentLetter == word.charAt(j)){
+                    repeatedLetters++;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < word.length(); i++){
+
+        }
+        int variations =
+
+    }
+
+}
